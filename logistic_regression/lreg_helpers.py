07f3dc8622621ea7fa2018fc9c29ecb87c73db70
phonosync <24829638+phonosync@users.noreply.github.com>
2020-03-02 13:09:17 +0100

    streamlined notebook added Theory section on Gradient Descent moved the 2d-plot stuff into the helper module

---
+++ logistic_regression/lreg_helpers.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def multivariateGrid(col_x, col_y, col_k, df, k_is_color=False, scatter_alpha=.5):
+    def colored_scatter(x, y, c=None):
+        def scatter(*args, **kwargs):
+            args = (x, y)
+            if c is not None:
+                kwargs['c'] = c
+            kwargs['alpha'] = scatter_alpha
+            plt.scatter(*args, **kwargs)
+
+        return scatter
+
+    g = sns.JointGrid(
+        x=col_x,
+        y=col_y,
+        data=df
+    )
+    color = None
+    legends=[]
+    for name, df_group in df.groupby(col_k):
+        legends.append(name)
+        if k_is_color:
+            color=name
+        g.plot_joint(
+            colored_scatter(df_group[col_x],df_group[col_y],color),
+        )
+        sns.distplot(
+            df_group[col_x].values,
+            ax=g.ax_marg_x,
+            color=color,
+        )
+        sns.distplot(
+            df_group[col_y].values,
+            ax=g.ax_marg_y,
+            color=color,            
+            vertical=True
+        )
+    # Do also global Hist:
+    sns.distplot(
+        df[col_x].values,
+        ax=g.ax_marg_x,
+        color='grey'
+    )
+    sns.distplot(
+        df[col_y].values.ravel(),
+        ax=g.ax_marg_y,
+        color='grey',
+        vertical=True
+    )
+    plt.legend(legends)
